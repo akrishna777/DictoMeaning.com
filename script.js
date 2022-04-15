140e32bd71e072a9606f44ee8abebacdbc5cd665
Aravind Krishna <akaravindk59@gmail.com>
2022-04-16 01:11:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,135 @@
+
+
+let submit = document.getElementById('searchForm')
+submit.addEventListener("submit", onFormSubmit)
+
+function onFormSubmit(e) {
+    e.preventDefault()
+    let word = document.getElementById('searchWord').value
+
+    function printData(data) {
+        let result = document.getElementById('results')
+        console.log(data[0].meanings[0].definitions.length)
+        if (data[0].meanings[0].definitions.length === 1) {
+            if (data[0].origin === undefined) {
+                result.innerHTML = `
+                <div class="container my-4" id="display">
+                    <h2><b>${data[0].word}</b></h2>
+                    
+                    <hr>
+                    <h3>Meaning</h3>
+                    <p><b>Part of Speech: </b><i>${data[0].meanings[0].partOfSpeech}</i> </p>
+                    <p><b>1.&nbsp </b>${data[0].meanings[0].definitions[0].definition} <br>
+                    <b>E.g. </b>${data[0].meanings[0].definitions[0].example} <br>
+                    <b>Synonyms:</b> <i>${data[0].meanings[0].definitions[0].synonyms[0]}, ${data[0].meanings[0].definitions[0].synonyms[1]}, ${data[0].meanings[0].definitions[0].synonyms[2]}</i></p>
+    
+                    <h3>Origin</h3>
+                    <p><i>Not Found</i></p>
+    
+                    <h3>Phonetics</h3>
+                    <p><i>${data[0].phonetic}</i>&nbsp<a href="${data[0].phonetics[0].audio}"  target="_blank"><span class="material-icons-outlined">
+                    volume_up
+                    </span></a></p>
+    
+                </div>
+            `
+
+            }
+            else {
+                result.innerHTML = `
+            <div class="container my-4" id="display">
+                    <h2><b>${data[0].word}</b></h2>
+                    
+                    <hr>
+                    <h3>Meaning</h3>
+                    <p><b>Part of Speech: </b><i>${data[0].meanings[0].partOfSpeech}</i> </p>
+                    <p><b>1.&nbsp </b>${data[0].meanings[0].definitions[0].definition} <br>
+                    <b>E.g. </b>${data[0].meanings[0].definitions[0].example} <br>
+                    <b>Synonyms:</b> <i>${data[0].meanings[0].definitions[0].synonyms[0]}, ${data[0].meanings[0].definitions[0].synonyms[1]}, ${data[0].meanings[0].definitions[0].synonyms[2]}</i></p>
+    
+                    <h3>Origin</h3>
+                    <p><i>${data[0].origin}</i></p>
+    
+                    <h3>Phonetics</h3>
+                    <p><i>${data[0].phonetic}</i>&nbsp<a href="${data[0].phonetics[0].audio}"  target="_blank"><span class="material-icons-outlined">
+                    volume_up
+                    </span></a></p>
+    
+                </div>
+            `
+
+            }
+
+
+
+        }
+        else {
+            if (data[0].origin === undefined) {
+                result.innerHTML = `
+                <div class="container my-4" id="display">
+                        <h2><b>${data[0].word}</b></h2>
+                        
+                        <hr>
+                        <h3>Meaning</h3>
+                        <p><b>Part of Speech: </b><i>${data[0].meanings[0].partOfSpeech}</i> </p>
+                        <p><b>1.&nbsp </b>${data[0].meanings[0].definitions[0].definition} <br>
+                        <b>E.g. </b>${data[0].meanings[0].definitions[0].example} <br>
+                        <b>Synonyms:</b> <i>${data[0].meanings[0].definitions[0].synonyms[0]}, ${data[0].meanings[0].definitions[0].synonyms[1]}, ${data[0].meanings[0].definitions[0].synonyms[2]}</i></p>
+                        <p><b>2.&nbsp </b>${data[0].meanings[0].definitions[1].definition} <br>
+                        <b>E.g. </b>${data[0].meanings[0].definitions[1].example}<br>
+                        <b>Synonyms:</b> <i>${data[0].meanings[0].definitions[1].synonyms[0]}, ${data[0].meanings[0].definitions[0].synonyms[1]}, ${data[0].meanings[0].definitions[0].synonyms[2]}</i></p>
+        
+                        <h3>Origin</h3>
+                        <p><i>Not Found</i></p>
+        
+                        <h3>Phonetics</h3>
+                        <p><i>${data[0].phonetic}</i>&nbsp<a href="${data[0].phonetics[0].audio}"  target="_blank"><span class="material-icons-outlined">
+                        volume_up
+                        </span></a></p>
+        
+                    </div>
+                `
+            }
+            else {
+                result.innerHTML = `
+                <div class="container my-4" id="display">
+                        <h2><b>${data[0].word}</b></h2>
+                        
+                        <hr>
+                        <h3>Meaning</h3>
+                        <p><b>Part of Speech: </b><i>${data[0].meanings[0].partOfSpeech}</i> </p>
+                        <p><b>1.&nbsp </b>${data[0].meanings[0].definitions[0].definition} <br>
+                        <b>E.g. </b>${data[0].meanings[0].definitions[0].example} <br>
+                        <b>Synonyms:</b> <i>${data[0].meanings[0].definitions[0].synonyms[0]}, ${data[0].meanings[0].definitions[0].synonyms[1]}, ${data[0].meanings[0].definitions[0].synonyms[2]}</i></p>
+                        <p><b>2.&nbsp </b>${data[0].meanings[0].definitions[1].definition} <br>
+                        <b>E.g. </b>${data[0].meanings[0].definitions[1].example}<br>
+                        <b>Synonyms:</b> <i>${data[0].meanings[0].definitions[1].synonyms[0]}, ${data[0].meanings[0].definitions[0].synonyms[1]}, ${data[0].meanings[0].definitions[0].synonyms[2]}</i></p>
+        
+                        <h3>Origin</h3>
+                        <p><i>${data[0].origin}</i></p>
+        
+                        <h3>Phonetics</h3>
+                        <p><i>${data[0].phonetic}</i>&nbsp<a href="${data[0].phonetics[0].audio}"  target="_blank"><span class="material-icons-outlined">
+                        volume_up
+                        </span></a></p>
+        
+                    </div>
+                `
+            }
+
+        }
+
+    }
+
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+        .then(response => response.json())
+        // .then(data => console.log(data))
+        .then(data => printData(data))
+        .catch(error => console.log('error', error));
+
+}
+
+function clear1() {
+    let result = document.getElementById('results')
+    result.innerHTML = ""
+}
